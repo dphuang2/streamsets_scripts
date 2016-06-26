758a8950db16f01e5e5ae6361f5e5c1bb283684f
Dylan Huang <dylan.p.huang@gmail.com>
2016-06-26 02:32:36 -0700

    Added correct indicator names for ONI dataset

--- JSEvalScripts/PoliticalScores.js
+++ JSEvalScripts/PoliticalScores.js
@@ -3,11 +3,32 @@ function writeRecord(Values, year, country, cc){
         var value = Values[key];
         // value : Political Score
         // key   : indicator
+        var ind;
+        switch(key){
+                case 'political_score':
+                    ind = "ONIp";
+                    break;
+                case 'social_score':
+                    ind = "ONIs";
+                    break;
+                case 'tools_score':
+                    ind = "ONIit";
+                    break;
+                case 'conflict/security_score':
+                    ind = "ONIcs";
+                    break;
+                case 'transparency':
+                    ind = "ONItr";
+                    break;
+                case 'consistency':
+                    ind = "ONIco";
+                    break;
+        }
+
         record = records[i] //Make new variable just called for easier reading
             // Note: this is a pass by reference so a new record is not created
-
             record.value = { 
-                '${ID}' : key,
+                '${ID}' : ind,
                 '${CT}' : cc,
                 '${DT}' : year,
                 '${VL}' : value,
@@ -25,7 +46,7 @@ for(var i = 0; i < records.length; i++) {
         var Values = {};
         for(key in records[i].value){
             var value = records[i].value[key];
-            if(key != 'country' && key != 'country_code' && key != 'testing_date' && key != 'url'){
+            if(key != 'social_description' && key != 'tools_description' && key != 'conflict_security_description' && key != 'political_description' && key != 'country' && key != 'country_code' && key != 'testing_date' && key != 'url'){
                 //records[i].value['Values'][key] = value;
                 Values[key] = value;
             }
