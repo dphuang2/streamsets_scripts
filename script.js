fd1f53605ae14a651bece64a37b5dd65f329909a
Dylan Huang <dylan.p.huang@gmail.com>
2016-08-21 20:54:04 -0500

    First draft done

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  var group_list = ".goals, .research, .script, .github";
+  var group_list = ".goals, .research, .system, .github, .redesign, .conclusion";
   $(group_list).css("display", "none");
   $(".goals").css("display", "block");
-  var button_list = "#goals, #research, #script, #github";
+  var button_list = "#goals, #research, #system, #github, #redesign, #conclusion";
   $(button_list).click(function(){
     var button_name = "#"+this.id;
     var group_name = "."+this.id;
